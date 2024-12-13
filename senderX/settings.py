5e67174ef301d693a9a229f8dfc0f3bfea5f8eb4
Timf <bmiron220@gmail.com>
2024-12-14 03:50:40 +0530

    'OK'

--- senderX/settings.py
+++ senderX/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +31,7 @@ TWILIO_PHONE_NUMBER = config('TWILIO_PHONE_NUMBER')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'https://senderxapp.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = [ 'https://senderxapp.onrender.com']
 
 
 # Application definition
