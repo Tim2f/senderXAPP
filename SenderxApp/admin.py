090ac6e71588947bab6b8ac0c40ad6bff9fbd987
iwebtrix <technologywebtree@gmail.com>
2024-12-29 16:31:58 +0530

    ok

--- SenderxApp/admin.py
+++ SenderxApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "SenderappXAdmin"
+admin.site.site_title = "senderxAdmin"
+admin.site.index_title = "Welcome to senderxAdmin Dashboard"
+
